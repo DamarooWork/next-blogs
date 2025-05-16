1e76a9315178a0b85314b9bfd30d8b803f690b7e
Damaroo <levkichigin@yandex.ru>
2025-05-16 20:41:35 +0300

    chore(prisma): add seed users and blogs

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,38 +5,62 @@ const prisma = new PrismaClient().$extends(withAccelerate())
 
 const userData: Prisma.UserCreateInput[] = [
   {
-    name: 'Alice',
-    email: 'alice1@prisma.io',
+    nickmane: 'Alice',
+    email: 'alice2@prisma.io',
+    password: 'alice',
+
     blogs: {
       create: [
         {
-          title: 'Join the Prisma Discord',
-          content: 'https://pris.ly/discord',
+          title: 'News 1',
+          content: ['Новые новости', 'Новые новости', 'Новые новости'],
+          previewImage:
+            'https://i.pinimg.com/736x/cb/6e/0e/cb6e0e9234d879cb482e96fde1ec4c29.jpg',
           published: true,
         },
         {
-          title: 'Prisma on YouTube',
-          content: 'https://pris.ly/youtube',
+          title: 'News 2',
+          content: ['Новые новости', 'Новые новости', 'Новые новости'],
+          previewImage:
+            'https://i.pinimg.com/736x/cb/6e/0e/cb6e0e9234d879cb482e96fde1ec4c29.jpg',
+
+          published: true,
         },
       ],
     },
   },
   {
-    name: 'Bob',
-    email: 'bob1@prisma.io',
+    nickmane: 'Bob',
+    email: 'bob2@prisma.io',
+    password: 'bob',
     blogs: {
       create: [
         {
-          title: 'Follow Prisma on Twitter',
-          content: 'https://www.twitter.com/prisma',
+          title: 'News 3',
+          content: ['Новые новости', 'Новые новости', 'Новые новости'],
+          previewImage:
+            'https://i.pinimg.com/736x/cb/6e/0e/cb6e0e9234d879cb482e96fde1ec4c29.jpg',
+          published: true,
+        },
+        {
+          title: 'News 4',
+          content: ['Новые новости', 'Новые новости', 'Новые новости'],
+          previewImage:
+            'https://i.pinimg.com/736x/cb/6e/0e/cb6e0e9234d879cb482e96fde1ec4c29.jpg',
           published: true,
         },
       ],
     },
-  }
+  },
 ]
-
+// async function down() {
+//   await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE`
+//   await prisma.$executeRaw`TRUNCATE TABLE "Blog" RESTART IDENTITY CASCADE`
+//   await prisma.$executeRaw`TRUNCATE TABLE "Comment" RESTART IDENTITY CASCADE`
+//   await prisma.$executeRaw`TRUNCATE TABLE "Like" RESTART IDENTITY CASCADE`
+// }
 export async function main() {
+  // await down()
   for (const u of userData) {
     await prisma.user.create({ data: u })
   }
